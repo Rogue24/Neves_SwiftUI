7e3f3814d5090d6bbd3f749c45ffec0789865046
zhoujianping24@hotmail.com <zhoujianping24@hotmail.com>
2021-12-10 16:39:45 +0800

    add test for frame

--- Neves_SwiftUI/View/Test/FrameTestView.swift
+++ Neves_SwiftUI/View/Test/FrameTestView.swift
@@ -4,12 +4,13 @@
 //
 //  Created by aa on 2021/12/9.
 //
+//  关于`VStack/HStack/ZStack`、`Image`的frame设置
 
 import SwiftUI
 
 struct FrameTestView: View {
     var body: some View {
-        ZStack {
+        ZStack(alignment: .top) {
             // SwiftUI默认始终采用图像的原始尺寸（图片多大就多大）
             Image(uiImage: UIImage.jp.fromBundle(forName: "Girl2", ofType: "jpg")!)
                 // 使用这个Modifier可以确保图片的【最小边】限制在父视图内
@@ -25,15 +26,23 @@ struct FrameTestView: View {
                 // 在.fill模式下，这样设置可以让图片`宽`和`高`都不会超出父视图区域
                 // PS：只要这个最小`宽/高`值小于等于父视图的`宽/高`即可
                 .frame(minWidth: 0, minHeight: 0) // 竖屏+横屏
-//                .frame(minWidth: 0) // 竖屏
-//                .frame(minHeight: 0) // 横屏
+//                .frame(minWidth: 0) // 仅处理竖屏
+//                .frame(minHeight: 0) // 仅处理横屏
             
+            Text("Hello")
+                .foregroundColor(.randomColor)
+                .padding()
         }
-        // 这样设置只会以子视图的大小
+        // 这样设置只会以子视图的大小（按最大的包裹）
 //        .frame(width: .infinity, height: .infinity)
         // 这样设置才会延伸至父视图的大小（如果子视图不足父视图的大小的情况下）
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous)) // 如果图片超出父视图区域就看不见这个圆角
+        
+        // 要设置frame再设置背景，否则背景的frame不是最新的frame（也就是CGSize.zero）
+        .background(Color.randomColor)
+        
+        // 如果图片超出父视图区域就看不见这个圆角
+        .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
     }
 }
 
