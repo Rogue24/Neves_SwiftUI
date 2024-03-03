2c553b61303762ee9de5d061fd25945a7c444e9b
zhoujianping <zhoujianping24@hotmail.com>
2024-03-03 22:49:31 +0800

    update

--- Neves_SwiftUI/View/Course/NetworkDemo/CodableTestView.swift
+++ Neves_SwiftUI/View/Course/NetworkDemo/CodableTestView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct CodableTestView: View {
     @State var text1: String = ""
     @State var text2: String = ""
+    @State var text3: String = ""
+    @State var text4: String = ""
     
     var body: some View {
         VStack(spacing: 20) {
@@ -24,24 +26,49 @@ struct CodableTestView: View {
             if text2.count > 0 {
                 Text(text2)
             }
+            
+            if text3.count > 0 {
+                Text(text3)
+            }
+            
+            if text4.count > 0 {
+                Text(text4)
+            }
         }
     }
 }
 
-
+private struct MyResponse<T: Decodable>: Decodable {
+    var status: Int
+    var quota: Int
+    var response: T
+}
 
 private extension CodableTestView {
     func doit() {
-        let user = try! JSONDecoder().decode(User.self, from: jsonData1)
-        let data = try! JSONEncoder().encode(user)
-        text1 = String(data: data, encoding: .utf8)!
-        
-        let rsp: Response<[User2]> = try! JSONDecoder().decode(Response.self, from: jsonData2)
-        text2 = "\(rsp)"
+        do {
+            let user = try JSONDecoder().decode(User.self, from: jsonData1)
+            let data = try JSONEncoder().encode(user)
+            text1 = String(data: data, encoding: .utf8)!
+            
+            let rsp1: MyResponse<[User2]> = try JSONDecoder().decode(MyResponse.self, from: jsonData2)
+            text2 = "\(rsp1)"
+            
+            let rsp2: MyResponse<User3> = try JSONDecoder().decode(MyResponse.self, from: jsonData3)
+            text3 = "\(rsp2)"
+            
+            let rsp3: MyResponse<User4> = try JSONDecoder().decode(MyResponse.self, from: jsonData3)
+            text4 = "\(rsp3)"
+        } catch {
+            text1 = "\(error.localizedDescription)"
+            text2 = ""
+            text3 = ""
+            text4 = ""
+        }
     }
 }
 
-// MARK: - 🌰1
+// MARK: - 🌰1 使用Enum的RawVallue声明不同名称
 
 private let jsonData1 = Data(
 """
@@ -84,7 +111,10 @@ extension CodableTestView.User: Encodable {
     }
 }
 
-// MARK: - 🌰2
+// MARK: - 🌰2 处理不同的命名惯用法（蛇形 <-> 驼峰）
+/// 备忘录中的`Course5_03`
+
+// MARK: - 🌰3 Nested数据（嵌套数据的处理）
 
 private let jsonData2 = Data(
 """
@@ -99,12 +129,6 @@ private let jsonData2 = Data(
 """.utf8)
 
 private extension CodableTestView {
-    struct Response<T: Decodable>: Decodable {
-        var status: Int
-        var quota: Int
-        var response: T
-    }
-    
     struct User2: Decodable {
         var id: Int
         var name: String
@@ -125,7 +149,7 @@ private extension CodableTestView {
     }
 }
 
-// MARK: - 🌰3
+// MARK: - 🌰4 解析 UnkeyedContainer（【字典】<->【数组】）
 
 private let jsonData3 = Data(
 """
@@ -173,13 +197,99 @@ private extension CodableTestView {
             var friendContainer = try container.nestedUnkeyedContainer(forKey: .friends)
             var fIDs: [Int] = []
             while !friendContainer.isAtEnd {
+                print("count: \(friendContainer.count ?? 0), currentIndex: \(friendContainer.currentIndex)")
+                /// 📢 注意1：
+                /// 由于`nestedContainer`是相当于【数组】->【字典】的类型变换操作，会改变`friendContainer`自身的数据结构，
+                /// 所以不能用`let`引用`friendContainer`，要用`var`。
                 let frienndIDContainer = try friendContainer.nestedContainer(keyedBy: FriendKeys.self)
                 fIDs.append(try frienndIDContainer.decode(Int.self, forKey: .id))
+                
+                /// 📢 注意2：
+                /// 这里通过判断`isAtEnd`来进行循环，【必须确保】有对`friendContainer`进行【解码】相关的操作，
+                /// 例如`nestedContainer`或`decode`等（`UnkeyedDecodingContainer`中有`mutating`声明的方法），
+                /// 每进行一次【解码】相关的操作`currentIndex`就会+1，直至`currentIndex`达到`count`所需次数，`isAtEnd`才变为`true`。
+                /// 可以打开以下注释即可观察区别：
+//                print("count: \(friendContainer.count ?? 0), currentIndex: \(friendContainer.currentIndex)")
+//                _ = try friendContainer.decode([String: Int].self)
+//                print("count: \(friendContainer.count ?? 0), currentIndex: \(friendContainer.currentIndex)")
+                /// 否则如果对`friendContainer`什么都不做，就会死循环！
             }
             self.friendIDs = fIDs
         }
     }
 }
+
+// MARK: - 🌰5
+
+private extension KeyedDecodingContainerProtocol {
+    // subscript的语法类似于实例方法、计算属性，本质就是方法(函数)
+    // subscript可以没有set方法，但必须要有get方法
+    
+    // associatedtype Key : CodingKey
+    subscript<T: Decodable>(_ key: Key) -> T { // ❌ -> throws T，subscript不能在函数的返回添加throws。
+        // 当需要在subscript或是属性中「抛出错误」，或者做「async」的时候，
+        // 必须要在里面去定义ta的【get】方法：
+        get throws {
+            try decode(T.self, forKey: key)
+        }
+    }
+}
+
+private extension UnkeyedDecodingContainer {
+    mutating func map<T>(_ transform: (Decoder) throws -> T) throws -> [T] {
+        var items: [T] = []
+        while !isAtEnd {
+            let item = try transform(superDecoder())
+            items.append(item)
+        }
+        return items
+    }
+}
+
+private extension CodableTestView {
+    struct User4: Decodable {
+        var id: Int
+        var name: String
+        var friendIDs: [Int]
+        
+        enum MyKeys: CodingKey {
+            case id, firstName, lastName, friends
+        }
+        
+        enum FriendKeys: CodingKey {
+            case id
+        }
+        
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: MyKeys.self)
+            
+            
+            self.id = try container[.id]
+            
+            let firstName: String = try container[.firstName]
+            let lastName: String = try container[.lastName]
+            self.name = firstName + " " + lastName
+            
+            // 现在的`container`是【字典】的形状，转成【数组】形状的`friendContainer`去遍历数据
+            // `nestedUnkeyedContainer(forKey:)`：【字典】->【数组】
+            var friendContainer = try container.nestedUnkeyedContainer(forKey: .friends)
+            self.friendIDs = try friendContainer.map { 
+//                friendDecoder in
+//                let frienndIDContainer = try friendDecoder.container(keyedBy: FriendKeys.self)
+//                return try frienndIDContainer[.id]
+                try $0.container(keyedBy: FriendKeys.self)[.id]
+            }
+            
+//            container.superDecoder().container(keyedBy: )
+            
+//            self.friendIDs = try container.decode([[String:Int]].self, forKey: .friends).map({ $0["id"] ?? 0 })
+            
+            
+            
+        }
+    }
+}
+
 
 #Preview {
     CodableTestView()
